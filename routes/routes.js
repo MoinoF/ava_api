e74f81804b80a985b45dc06aa714403c33d9cc3c
scogo <scogoboy79@gmail.com>
2024-05-23 15:06:37 +0200

    Plan Create Controller modified

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const HomeController = require('../controllers/HomeController')
+const PlanCreateController = require('../controllers/plans/PlanCreateController')
 const router = express.Router()
 
+/**
+ * Home
+ */
 router.get('/', HomeController.execute)
+
+/**
+ * Plans
+ */
+router.post('/api/plans/create', PlanCreateController.create )
 
 
 
